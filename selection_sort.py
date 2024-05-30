7b33fd56788d4f6a35a88dbe02df6e5e4c929030
aditya1997-debug <77202991+aditya1997-debug@users.noreply.github.com>
2024-05-30 15:48:12 +0530

    Add files via upload
    Use selection sort and save changes in an array

---
+++ selection_sort.py
@@ -0,0 +1,18 @@
+nums = [7, 5, 9, 2, 8]
+
+result = []
+
+for i in range(0, len(nums)):
+    mini = nums[i]
+    for j in range(i, len(nums)):
+        if nums[j] < mini:
+            mini = nums[j]
+            nums[i], nums[j] = nums[j], nums[i]
+        # print(nums)
+        result.append(nums)
+    # print(result[0])
+
+
+print("States ", result)
+print("=================>")
+print("Sorted array", nums)
