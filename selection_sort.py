d9a8ab8b7a074feb0741e8f93ec21e3f99302c75
aditya1997-debug <77202991+aditya1997-debug@users.noreply.github.com>
2024-05-30 15:33:35 +0530

    Implementation of selection sort and saving states in an array

---
+++ selection_sort.py
@@ -0,0 +1,16 @@
+arr = [5,4,3,2,1]
+result = []
+
+for i in range(len(arr)):
+    for j in range(len(arr)):
+        sub_arr = arr[j : j + 2]
+        if len(sub_arr) < 2:
+            break
+        if sub_arr[1] < sub_arr[0]:
+            arr[j], arr[j + 1] = arr[j + 1], arr[j]
+        result.append(arr[:])
+
+
+print("States ", result)
+print("====================>")
+print("Sorted Array", result[-1])
